58a06ec47363e1ec2459e476d6c32974c5939772
faiqnoov <mfaiqn@gmail.com>
2023-02-03 01:34:38 +0700

    bismillah init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel 9</title>
+    </head>
+    <body>
+        <div>Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</div>
+    </body>
+</html>
